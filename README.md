56580c58eb1c3970a3f289cd6a0dca0a6761f023
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 <vins25165@gmail.com>
2021-10-08 21:31:27 +0800

    redesign web UI/UX - use SWA

---
+++ README.md
@@ -0,0 +1,31 @@
+![web-view](./img/web-view.png)
+
+# Vincent Saerang - Portofolio
+This website for my portofolio, this code is free to use.
+this is single web app (SWA) made with Vanilla Javascript, CSS, HTML 
+
+# Make with
+- HTML
+- CSS
+- JavaScript
+- easy-doc
+- font awesome v5.15
+
+# File structure
+```
+> css
+>> style.css
+
+> img
+>> A LOT OF IMAGE
+
+> js
+>> script.js
+
+> index.html
+> README.md (this)
+```
+
+## give a star pls
+
+# Current version 1.0.0
---
+++ js/script.js
@@ -0,0 +1,76 @@
+let navST = getDocById("st");
+let navH = getDocById("h");
+let navSM = getDocById("sm");
+let lPG = getDocById("loading-pg");
+
+window.onload = () => {
+	setTimeout(() => {
+		addClassTo(lPG, ["loaded"]);
+	}, 1000)
+}
+
+function showRemoveLoading(time) {
+	removeClassFrom(lPG, ["loaded"]);
+
+	setTimeout(() => {
+		addClassTo(lPG, ["loaded"]);
+	}, time)
+}
+
+let navList = [
+	{
+		name: "skills-tools",
+		nav: navST
+	},
+	{
+		name: "home",
+		nav: navH
+	},
+	{
+		name: "social-media",
+		nav: navSM
+	}
+]
+
+function navigateTo(id) {
+	let navToPage = getDocById(id);
+	if (!navToPage) return console.log("cannot find the page")
+
+
+	let current = getDocsByClass("active-page");
+	if (!current) return console.log("cannot find current page");
+	showRemoveLoading(1000);
+
+	let currentPg = current[0];
+
+	removeClassFrom(currentPg, ["active-page"]);
+
+	setTimeout(() => {
+		document.title = `Vincent Saerang - ${id}`;
+
+		addClassTo(navToPage, ["active-page"]);
+	}, 500)
+
+	removeClassFrom(navST, ["active"])
+	removeClassFrom(navH, ["active"])
+	removeClassFrom(navSM, ["active"])
+
+	let targetActive = navList.find(x => x.name === id);
+
+
+	addClassTo(targetActive.nav, ["active"])	
+}
+
+function copyEmail(e) {
+	e.innerHTML = "Copied";
+	navigator.clipboard.writeText("vins25165@gmail.com")
+	setTimeout(() => {
+		e.innerHTML = "Copy Email"
+	}, 2000)
+}
+
+function openLink(link = []) {
+	link.forEach(l => {
+		window.open(l, "_blank")
+	})
+}
